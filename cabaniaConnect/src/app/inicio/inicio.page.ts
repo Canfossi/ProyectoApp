e2e7c4a7a1f5f04318c7243e58443772244607fb
carloAnfo <c.anfossi@duocuc.cl>
2023-09-12 19:38:27 -0300

    cambio de inicio ahora puedes cerrar sesion

--- cabaniaConnect/src/app/inicio/inicio.page.ts
+++ cabaniaConnect/src/app/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController,NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private alertCtrl: AlertController,//controlar alerts
+    private navCtrl: NavController,//controlar los links
+    
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+
+    
   }
 
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Cerrar sesión',
+      message: '¿Está seguro de querer cerrar la sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          },
+        },
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Cerrar sesión');
+            localStorage.removeItem('user'); // Verifica que esto esté funcionando
+            this.navCtrl.navigateForward('/login'); // Redirecciona a la página de inicio de sesión
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+  
 }
